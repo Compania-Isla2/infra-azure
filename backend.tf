e5490f015d6ff18daa5134dcc01679724c819c53
CIS <chrismindedness@gmail.com>
2025-09-14 13:07:10 -0500

    cambio de apellido

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-cicd-terraform-app-apellido" # Reemplazar por apellido
-    storage_account_name = "tfstateapellido"                # Reemplazar por apellido
+    resource_group_name  = "rg-cicd-terraform-app-cisla" # Reemplazar por cisla
+    storage_account_name = "tfstatecisla"                # Reemplazar por cisla
     container_name       = "tfstate"
     key                  = "dev/terraform.tfstate"
   }
